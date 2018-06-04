9f0fc06621e26a2c3faa101714999b1ac719d897
Damir Perkosan <damir.perkosan@diwanee.com>
2018-06-04 11:47:43 +0200

    testing

---
+++ tests/RatingTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Description of RatingTest
+ *
+ * @author damir
+ */
+class RatingTest  extends Orchestra\Testbench\TestCase
+{
+    public function testSomethingIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
